174c51357e40b6eefa77efb0c8d9166d17bc486c
Thomas Auinger <thomas@auinger.de>
2022-04-07 16:01:10 +0200

    added javadoc

--- src/main/java/de/auinger/training/rest/server/RestfulApplication.java
+++ src/main/java/de/auinger/training/rest/server/RestfulApplication.java
@@ -3,6 +3,9 @@ package de.auinger.training.rest.server;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ * Having this allows us to not create a web.xml
+ */
 @ApplicationPath("/api")
 public class RestfulApplication extends Application {
 }
