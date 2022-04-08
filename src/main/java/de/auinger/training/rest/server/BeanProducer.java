b0ca37fc9e628fe13dd1ab02789397805d6a7e71
Thomas Auinger <thomas@auinger.de>
2022-04-08 10:03:12 +0200

    fixed missing store-service; added get-single-book

--- src/main/java/de/auinger/training/rest/server/BeanProducer.java
+++ src/main/java/de/auinger/training/rest/server/BeanProducer.java
@@ -1,13 +1,35 @@
 package de.auinger.training.rest.server;
 
-import javax.enterprise.inject.Produces;
 import org.javacream.books.warehouse.api.BooksService;
 import org.javacream.books.warehouse.impl.MapBooksService;
+import org.javacream.store.api.StoreService;
+import org.javacream.store.impl.StoreServiceImpl;
+
+import javax.enterprise.inject.Produces;
 
 public class BeanProducer {
 
+    // --- initialize and store dependencies:
+
+    private final StoreServiceImpl storeService;
+    private final MapBooksService booksService;
+
+    private BeanProducer() {
+        storeService = new StoreServiceImpl();
+
+        booksService = new MapBooksService();
+        booksService.setStoreService(storeService);
+    }
+
+    // --- make dependencies known to CDI context:
+
     @Produces
     public BooksService booksService() {
-        return new MapBooksService();
+        return booksService;
     }
+
+	@Produces
+	public StoreService storeService() {
+		return storeService;
+	}
 }
--- src/main/java/de/auinger/training/rest/server/book/BooksResource.java
+++ src/main/java/de/auinger/training/rest/server/book/BooksResource.java
@@ -1,25 +1,33 @@
 package de.auinger.training.rest.server.book;
 
 import org.javacream.books.warehouse.api.Book;
+import org.javacream.books.warehouse.api.BookException;
 import org.javacream.books.warehouse.api.BooksService;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
 
 @Path("/books")
+@Produces(MediaType.APPLICATION_JSON)
 public class BooksResource {
 
     @Inject
     private BooksService booksService;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Book> getBooks() {
         return new ArrayList<>(booksService.findAllBooks());
+    }
+    
+    @GET
+    @Path("/{isbn}")
+    public Book getBook(@PathParam("isbn") String isbn) throws BookException {
+    	return booksService.findBook(isbn);
     }
 }
