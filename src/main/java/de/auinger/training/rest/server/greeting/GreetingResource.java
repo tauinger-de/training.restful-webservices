ed97a496aaebb957d0121856e5b06a2e97fe7e1e
Thomas Auinger <thomas@auinger.de>
2022-04-08 12:59:45 +0200

    GreetingResource can now deal with firstName given by QUERY, HEADER or PATH

--- src/main/java/de/auinger/training/rest/server/greeting/GreetingResource.java
+++ src/main/java/de/auinger/training/rest/server/greeting/GreetingResource.java
@@ -1,29 +1,52 @@
 package de.auinger.training.rest.server.greeting;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.Random;
 
 @Path("/greeting")
 public class GreetingResource {
 
+    // jax-rs creates an instance of this class FOR EACH REQUEST -- hence we can do stuff like this:
+    @QueryParam("firstName") String firstNameFromQuery;
+    @HeaderParam("X-firstName") String firstNameFromHeader;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getGreeting() {
-        return generateGreeting();
+        // which name to use? header wins over query!
+        String firstName = (firstNameFromHeader != null) ? firstNameFromHeader : firstNameFromQuery;
+        return generateGreeting(firstName);
     }
 
-    private String generateGreeting() {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{firstName}")
+    public String getGreetingUsingPath(@PathParam("firstName") String firstName) {
+        return generateGreeting(firstName);
+    }
+
+    private String generateGreeting(String firstName) {
+        String greeting;
         switch (new Random().nextInt(3)) {
             case 0:
-                return "Hello";
+                greeting = "Hello";
+                break;
             case 1:
-                return "Que tal";
+                greeting = "Que tal";
+                break;
             case 2:
             default:
-                return "Guten Tag";
+                greeting = "Guten Tag";
         }
+        if (firstName != null && !firstName.isEmpty()) {
+            greeting = greeting + " " + firstName;
+        }
+        return greeting;
     }
 }
